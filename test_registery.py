73039a3c61786c34f22964ed82b68ae9a28dd30d
Yusin Chen <chenyuxing16@mails.ucas.ac.cn>
2024-10-28 10:57:17 +0100

    Add files via upload

--- test_registery.py
+++ test_registery.py
@@ -1,14 +1,13 @@
 from datetime import datetime, timezone
 from dataApi.gee_utils import BBox
-from dataApi.geeData_tool import geeData_registery
+from dataApi.geeAlldata_tool import geeData_registery
 from modelApi.samgeo_tools import samGeo_registry
 
 print('data modules')
 print(geeData_registery.functions)
-print(geeData_registery.functions['S1_GRD.get_S1_GRD'])
 test_geeData = geeData_registery.to_list_infos(query_bbox=BBox(2.3358203, 48.8421609, 2.3709914, 48.8624786),
                                                query_interval=(datetime(2020, 5, 17).replace(tzinfo=timezone.utc), datetime(2024, 5, 17).replace(tzinfo=timezone.utc)),
-                                               sensor='Sentinel-1')
+                                               sensor=None)
 print('gee test2', test_geeData)
 #import inspect
 #print(inspect.getsource(test_geeData[0][1]))
